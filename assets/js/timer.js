ea3112c158c0296d8b1e6409685e2c33330e6382
Eric Cheng <echeng@scu.edu>
2020-07-13 13:01:19 -0700

    Add working clock, signal buttons, and color change

---
+++ assets/js/timer.js
@@ -0,0 +1,11 @@
+function GetClock(){
+    var d=new Date();
+    var nhour=d.getHours(),nmin=d.getMinutes();
+    if(nmin<=9) nmin="0"+nmin
+
+    var clocktext=""+nhour+":"+nmin+"";
+    document.getElementById('time').innerHTML=clocktext;
+    }
+
+    GetClock();
+    setInterval(GetClock,1000);
